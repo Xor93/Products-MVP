32e189810bb9aad2fbf976883953eb9dc43d12f0
Ниджат Ханахмедов <qwertyq3000@gmail.com>
2019-12-17 00:39:03 +0400

    Add project files.

---
+++ ShopMVP/Program.cs
@@ -0,0 +1,38 @@
+﻿using ShopMVP.Models;
+using ShopMVP.Presenters;
+using ShopMVP.Services;
+using ShopMVP.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShopMVP
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            #region IOC setting
+            IOC.Register<ShopForm, IShopView>();
+            IOC.Register<ProductForm, IProductView>();
+            IOC.Register<XmlProductService, IProductService>();
+            IOC.Register<ShopPresenter>();
+            IOC.Register<ProductPresenter>();
+            IOC.Build();
+            #endregion
+
+            ShopPresenter shopPresenter = IOC.Resolve<ShopPresenter>();
+
+            Application.Run((Form)shopPresenter.View);
+        }
+    }
+}
